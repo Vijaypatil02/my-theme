8fb393dfe0cf208a301efe3dbfcf060c07ed3844
VIJAY PATIL <78361332+Vijaypatil02@users.noreply.github.com>
2021-03-04 14:39:39 +0530

    minor theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashbordComponent} from './dashbord/dashbord.component';
+import {ButtonsComponent} from './buttons/buttons.component';
+import {CardsComponent} from './cards/cards.component';
+import {AnimationsComponent} from './animations/animations.component';
+import {OtherComponent} from './other/other.component';
+import {ColorsComponent} from './colors/colors.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ForgotpasswordComponent} from './forgotpassword/forgotpassword.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {BlankpageComponent} from './blankpage/blankpage.component';
+import {ChartsComponent} from './charts/charts.component';
+import {TablesComponent} from './tables/tables.component';
+import {LogoutComponent} from './logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component : DashbordComponent},
+  {path: 'buttons' , component : ButtonsComponent},
+  {path: 'cards' , component : CardsComponent},
+  {path: 'animations' , component : AnimationsComponent},
+  {path: 'borders' , component : ButtonsComponent},
+  {path: 'other' , component : OtherComponent},
+  {path: 'colors' , component : ColorsComponent},
+  {path: 'login' , component : LoginComponent},
+  {path: 'register' , component : RegisterComponent},
+  {path: 'forgotpassword' , component : ForgotpasswordComponent},
+  {path: 'notfound' , component : NotfoundComponent},
+  {path: 'blankpage' , component : BlankpageComponent},
+  {path: 'charts' , component : ChartsComponent},
+  {path: 'tables' , component : TablesComponent},
+  {path: 'logout' , component : LogoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideComponent } from './side/side.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
+import { ColorsComponent } from './colors/colors.component';
+import { BordersComponent } from './borders/borders.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { OtherComponent } from './other/other.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BlankpageComponent } from './blankpage/blankpage.component';
+import { ChartsComponent } from './charts/charts.component';
+import { TablesComponent } from './tables/tables.component';
+import { LogoutComponent } from './logout/logout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideComponent,
+    ContentComponent,
+    FooterComponent,
+    DashbordComponent,
+    ButtonsComponent,
+    CardsComponent,
+    ColorsComponent,
+    BordersComponent,
+    AnimationsComponent,
+    OtherComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotpasswordComponent,
+    NotfoundComponent,
+    BlankpageComponent,
+    ChartsComponent,
+    TablesComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
